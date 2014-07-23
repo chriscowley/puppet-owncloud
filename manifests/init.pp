3bb08448107a2b9b147cbb810a5d18f785dd1d3d
¨Chris <ccowley@b-com.com>
2014-07-23 16:49:35 +0200

    Tweaked Nginx Config

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,16 +8,16 @@
 #   Explanation of what this parameter affects and what it defaults to.
 #
 class owncloud (
-  $filename   = $owncloud::params::filename,
-  $url        = $owncloud::params::url,
-  $servername = $owncloud::params::servername,
-  $webserver  = $owncloud::params::webserver,
-  $ssl        = $owncloud::params::ssl,
+  $filename      = $owncloud::params::filename,
+  $url           = $owncloud::params::url,
+  $servername    = $owncloud::params::servername,
+  $webserver     = $owncloud::params::webserver,
+  $ssl           = $owncloud::params::ssl,
   $dbtype        = $owncloud::params::dbtype,
-  $dbname        = $owncloud::params::$dbname,
-  $dbuser        = $owncloud::params::$dbuser,
-  $dbpassword    = $owncloud::params::$dbpassword,
-  $dbtableprefix = $owncloud::params::$dbtableprefix,
+  $dbname        = $owncloud::params::dbname,
+  $dbuser        = $owncloud::params::dbuser,
+  $dbpassword    = $owncloud::params::dbpassword,
+  $dbtableprefix = $owncloud::params::dbtableprefix,
 ) inherits owncloud::params {
 
   # validate parameters here
